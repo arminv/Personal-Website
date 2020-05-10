b8ddfd523aef496811bedef0ceba02172bfe60d1
arminv <arminv@users.noreply.github.com>
2020-05-10 03:21:47 -0400

    Added new project (covid-19)

--- src/assets/data/projectData.js
+++ src/assets/data/projectData.js
@@ -31,6 +31,33 @@ export default [
     link: "https://note-keeper-final.firebaseapp.com/register"
   },
   {
+    title: "COVID-19 Dashboard",
+    description: `
+            Dashboard to keep track of COVID-19
+            `,
+    src: "covid.png",
+    tags: [
+      [
+        "whitesmoke",
+        "React",
+        "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
+      ],
+      ["whitesmoke", "Chartjs", "https://www.chartjs.org/img/chartjs-logo.svg"],
+      [
+        "whitesmoke",
+        "RapidAPI",
+        "https://rapidapi.com/static-assets/default/logo.svg"
+      ],
+      [
+        "whitesmoke",
+        "Netlify",
+        "https://avatars0.githubusercontent.com/u/7892489?s=200&v=4"
+      ]
+    ],
+    github: "https://github.com/arminv/COVID-19-React",
+    link: "https://covid-19-dashboard-react.netlify.app/"
+  },
+  {
     title: "My Personal Website",
     description: `
             This website!
