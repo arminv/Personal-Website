f8c7a37f20138eb20841499289fd23715ac8767d
arminv <arminv@users.noreply.github.com>
2020-05-10 03:36:00 -0400

    Added links to experience page items

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -10,38 +10,46 @@
         vs-offset="3"
       >
         <div class="timeline">
-          <vue-timeline-update
-            :date="new Date('2019-08-01')"
-            title="Product Manager | Front-End Developer"
-            description="<li>Overseeing feature development</li><li>Reporting (via JIRA) and fixing bugs (in React)</li><li>Coordinating with remote teams</li><li>Quality Assurance (QA)</li>"
-            icon="code"
-            color="red"
-            :thumbnail="resolve_img_url('finneo.png')"
-          />
-          <vue-timeline-update
-            :date="new Date('2018-12-01')"
-            title="Financial Analyst"
-            description="<li>Commercial real estate underwriting</li><li>Optimized capital structure of various projects (construction, land, etc.)</li><li>Helped clients secure $20MM in commercial real estate funds (mortgages)</li>"
-            icon="edit"
-            color="red"
-            :thumbnail="resolve_img_url('r2.png')"
-          />
-          <vue-timeline-update
-            :date="new Date('2018-11-01')"
-            title="Volunteer - PyCon Canada"
-            icon="flag"
-            color="red"
-            :thumbnail="resolve_img_url('pycon.png')"
-          />
-          <vue-timeline-update
-            :date="new Date('2018-11-01')"
-            title="Open-Source Contributor - Pandas"
-            icon="code"
-            color="red"
-            :thumbnail="resolve_img_url('pandas.png')"
-            description="<li>Got all 4 of my Pull Requests merged into pandas</li>"
-            isLast
-          />
+          <a href="https://finneotech.com">
+            <vue-timeline-update
+              :date="new Date('2019-08-01')"
+              title="Product Manager | Developer"
+              description="<li>Overseeing feature development</li><li>Reporting (via JIRA) and fixing bugs (in React)</li><li>Coordinating with remote teams</li><li>Quality Assurance (QA)</li>"
+              icon="code"
+              color="red"
+              :thumbnail="resolve_img_url('finneo.png')"
+            />
+          </a>
+          <a href="http://capstack.ca">
+            <vue-timeline-update
+              :date="new Date('2018-12-01')"
+              title="Financial Analyst"
+              description="<li>Commercial real estate underwriting</li><li>Optimized capital structure of various projects (construction, land, etc.)</li><li>Helped clients secure $20MM in commercial real estate funds (mortgages)</li>"
+              icon="edit"
+              color="red"
+              :thumbnail="resolve_img_url('capstack.png')"
+            />
+          </a>
+          <a href="https://2019.pycon.ca">
+            <vue-timeline-update
+              :date="new Date('2018-11-01')"
+              title="Volunteer - PyCon Canada"
+              icon="flag"
+              color="red"
+              :thumbnail="resolve_img_url('pycon.png')"
+            />
+          </a>
+          <a href="https://pandas.pydata.org">
+            <vue-timeline-update
+              :date="new Date('2018-11-01')"
+              title="Open-Source Contributor - Pandas"
+              icon="code"
+              color="red"
+              :thumbnail="resolve_img_url('pandas.png')"
+              description="<li>Got all 4 of my Pull Requests merged into pandas</li>"
+              isLast
+            />
+          </a>
         </div>
       </vs-col>
     </vs-row>
