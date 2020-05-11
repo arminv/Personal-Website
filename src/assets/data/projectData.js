19d3793b35de884ba8d1091b2860b60637b1f992
arminv <arminv@users.noreply.github.com>
2020-05-10 22:40:36 -0400

    Added Material-UI badge to project

--- src/assets/data/projectData.js
+++ src/assets/data/projectData.js
@@ -33,7 +33,7 @@ export default [
   {
     title: "COVID-19 Dashboard",
     description: `
-            Dashboard to keep track of COVID-19
+            Dashboard to keep track of spread of COVID-19
             `,
     src: "covid.png",
     tags: [
@@ -43,6 +43,11 @@ export default [
         "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
       ],
       ["whitesmoke", "Chartjs", "https://www.chartjs.org/img/chartjs-logo.svg"],
+      [
+        "whitesmoke",
+        "Material-UI",
+        "https://material-ui.com/static/logo_raw.svg"
+      ],
       [
         "whitesmoke",
         "RapidAPI",
@@ -60,7 +65,7 @@ export default [
   {
     title: "My Personal Website",
     description: `
-            This website!
+            This website
             `,
     src: "personal.png",
     tags: [
