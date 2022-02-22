9d0adc50bd947d9b821c7fb17f2b1b9306a063dc
arminv <arminv@users.noreply.github.com>
2022-02-22 03:31:59 -0500

    Update latest job description

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -18,8 +18,14 @@
           >
             <vue-timeline-update
               :date="new Date('2021-01-25')"
-              title="Frontend Engineer"
-              description=""
+              title="Front End Engineer II"
+              description="<li>Implemented new features (using Lit, TypeScript, and Redux) while working closely with Product Owners and Designers</li>
+              <li>Added unit tests to increase overall code coverage and help achieve team and departmental OKRs</li>
+              <li>Refactored and optimized multiple product features as the only FE engineer on the Scrum team</li>
+              <li>Maintained legacy code written in AngularJS and Mithril.js</li>
+              <li>Authored technical and non-technical documentation (internal wiki) to explain new features and clarify any regressions</li>
+              <li>Participated in Design and Analysis (D&A) meetings to brainstorm, provide estimates on new epics and their feasibility, and help T-shirt size future work</li>
+              <li>Showcased Scrum team's work to the whole company during demos</li>"
               icon="code"
               color="red"
               :thumbnail="resolve_img_url('SenseiLabs.png')"
@@ -32,9 +38,9 @@
             rel="noopener noreferrer"
           >
             <vue-timeline-update
-              :date="new Date('2020-12-01')"
-              title="Product Manager / Full Stack Developer"
-              description="<li>Fixed bugs and improved overall user experience (B2B SaaS application – tech stack: React, Redux, Redux-Saga, Node, Feathers JS, MongoDB, PostgreSQL)</li><li>Collaborated with both internal and external software teams to launch features
+              :date="new Date('2019-12-01')"
+              title="Full Stack Developer / Product Manager"
+              description="<li>Fixed bugs and improved overall user experience (B2B SaaS application - tech stack: React, Redux, Redux-Saga, Node, Feathers JS, MongoDB, PostgreSQL)</li><li>Collaborated with both internal and external software teams to launch features
               on time and within budget</li><li>Performed software testing to uncover bugs and troubleshoot issues prior to
               application launch</li><li>Developed product documentation to communicate key components of
               upcoming features to software teams</li><li>Collaborated on all stages of development lifecycle, from requirement
@@ -45,21 +51,15 @@
               :thumbnail="resolve_img_url('finneo.png')"
             />
           </a>
-          <a
-            href="http://capstack.ca"
-            target="_blank"
-            alt="capstack"
-            rel="noopener noreferrer"
-          >
-            <vue-timeline-update
-              :date="new Date('2018-12-01')"
-              title="Financial Analyst"
-              description="<li>Commercial real estate underwriting</li><li>Originated, reviewed, processed, closed and administered customer loan proposals</li><li>Assisted clients with securing $20MM in commercial real estate funds</li><li>Developed and maintained client relationships</li>"
-              icon="edit"
-              color="red"
-              :thumbnail="resolve_img_url('capstack.png')"
-            />
-          </a>
+
+          <vue-timeline-update
+            :date="new Date('2018-12-01')"
+            title="Financial Analyst"
+            description="<li>Commercial real estate underwriting</li><li>Originated, reviewed, processed, closed and administered customer loan proposals</li><li>Assisted clients with securing $20MM in commercial real estate funds</li><li>Developed and maintained client relationships</li>"
+            icon="edit"
+            color="red"
+            :thumbnail="resolve_img_url('capstack.png')"
+          />
           <a
             href="https://2019.pycon.ca"
             target="_blank"
